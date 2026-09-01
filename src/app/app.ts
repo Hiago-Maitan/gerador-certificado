8fad9e3ee99d5c7396ec5eaf8144d8e4f17b2d1b
Hiago Maitan Silva <EM2026009519@nfoque.local>
2026-09-01 16:24:23 -0300

    componentes: button e item-certificado

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { CommonModule } from '@angular/common';
+import { PrimaryButton } from './_components/primary-button/primary-button';
+import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificado } from './_components/item-certificado/item-certificado';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, CommonModule],
+  imports: [RouterOutlet, Navbar, CommonModule, PrimaryButton, SecondaryButton, ItemCertificado],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
